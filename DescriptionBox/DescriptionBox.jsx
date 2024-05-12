a6fa34d645859e75568cb1ee204ec11610b64040
Camp4er <143322030+Camp4er@users.noreply.github.com>
2024-05-12 22:24:38 +0530

    Add files via upload

---
+++ DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='decriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>An e-commerce website is an online platform where businesses and consumers engage in buying and selling goods or services electronically. It provides a virtual marketplace where products or services are displayed, allowing customers to browse, compare, and purchase items from the comfort of their homes. These websites typically offer a wide range of products, spanning various categories such as clothing, electronics, home goods, and more. Users can easily search for specific items, read product descriptions and reviews, and complete transactions securely through integrated payment gateways. E-commerce websites revolutionize traditional retail, offering convenience, accessibility, and a global reach to both sellers and buyers alike.</p>
+        <p>In addition to facilitating transactions, e-commerce websites often feature personalized user accounts, enabling customers to track orders, save preferences, and receive tailored recommendations based on their browsing history. They employ advanced technologies like secure payment gateways, encryption, and fraud detection systems to ensure the safety of online transactions. E-commerce platforms also foster a competitive marketplace, allowing businesses of all sizes to reach a global audience and compete on a level playing field. With the rise of mobile shopping, these websites are increasingly optimized for mobile devices, offering seamless browsing and purchasing experiences across different screens. Overall, e-commerce websites revolutionize the way people shop, offering convenience, accessibility, and endless possibilities in the digital marketplace.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
